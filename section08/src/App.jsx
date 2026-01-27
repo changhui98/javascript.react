fa2ba681e9c97358071152d2c22958d704b2b6e5
changhui98 <net_0102@naver.com>
2026-01-27 22:12:29 +0900

    Study : 기능 구현 준비

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    data: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    data: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    data: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
